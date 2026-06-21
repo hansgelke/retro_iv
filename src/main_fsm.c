e54b6f5a5464c2d868406902634b3c3d4044247e
hansgelke <hans@gelke.ch>
2026-06-21 18:11:29 +0200

    SPIO Adress Mux

---
+++ src/main_fsm.c
@@ -0,0 +1,5 @@
+//
+// Created by Hans Gelke on 21.06.2026.
+//
+
+#include "main_fsm.h"
---
+++ src/main_fsm.h
@@ -0,0 +1,8 @@
+//
+// Created by Hans Gelke on 21.06.2026.
+//
+
+#ifndef SRC_MAIN_FSM_H
+#define SRC_MAIN_FSM_H
+
+#endif //SRC_MAIN_FSM_H
